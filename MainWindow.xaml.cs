b62809003bf62947214d729a321c4f2617cbf09e
Scathak <scathakocridera@gmail.com>
2024-10-22 20:42:25 +0300

    Created WPF app with 11 windows.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS_restaurant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenLoginWindow();
+        }
+        private void OpenLoginWindow()
+        {
+            LoginLogoutWindow loginWindow = new LoginLogoutWindow();
+            loginWindow.Show();
+            this.Close();
+        }
+    }
+}
