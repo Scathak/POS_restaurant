5cd7c419bb07b3fbe0bbd4d83656c539eebacf91
Scathak <scathakocridera@gmail.com>
2024-11-17 18:32:43 +0300

    Complimentary inventory data structure was added. Products inventory data structure was added.

--- ComplimentaryInventoryWindow.xaml.cs
+++ ComplimentaryInventoryWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static POS_restaurant.MenuManagementWindow;
 
 namespace POS_restaurant
 {
@@ -19,10 +22,37 @@ namespace POS_restaurant
     /// </summary>
     public partial class ComplimentaryInventoryWindow : Window
     {
+        // in millimiters
+        public class ItemSize
+        {
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public int Length { get; set; }
+        }
+        public class ComplimentaryInventoryRecord
+        {
+            [Key]
+            public int Number { get; set; }
+            public string Name { get; set; }
+            public ItemSize Size { get; set; }
+            public double Weight { get; set; }
+            public int QuantityInStock { get; set; }
+            public decimal InitialPrice { get; set; }
+            public string Currency { get; set; }
+            public string EncountingDate { get; set; }
+        }
+        public ObservableCollection<ComplimentaryInventoryRecord> ComplimentaryInventoryCollection { get; set; }
         public ComplimentaryInventoryWindow()
         {
             InitializeComponent();
             MainHeader.SetWindowName("3. Inventory of commercial equipment");
+            ComplimentaryInventoryCollection = [
+                new ComplimentaryInventoryRecord(){Number = 1, Name = "Table", Size = new ItemSize(){Height = 1200, Width = 750, Length = 600}, Weight = 1.5, QuantityInStock = 10, InitialPrice = 800, Currency = "TL", EncountingDate = "01/01/2024" },
+                new ComplimentaryInventoryRecord(){Number = 2, Name = "Chair", Size = new ItemSize(){Height = 850, Width = 600, Length = 600}, Weight = 0.8, QuantityInStock = 5, InitialPrice = 450, Currency = "TL", EncountingDate = "01/01/2024" },
+                new ComplimentaryInventoryRecord(){Number = 3, Name = "Fridge", Size = new ItemSize(){Height = 2000, Width = 800, Length = 800}, Weight = 35, QuantityInStock = 2, InitialPrice = 3000, Currency = "TL", EncountingDate = "01/01/2024" },
+            ];
+
+            ComplimentaryInventoryDataGrid.ItemsSource = ComplimentaryInventoryCollection;
         }
     }
 }
--- FoodInventoryWindow.xaml.cs
+++ FoodInventoryWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Windows;
+using static POS_restaurant.ComplimentaryInventoryWindow;
 
 namespace POS_restaurant
 {
@@ -7,10 +10,28 @@ namespace POS_restaurant
     /// </summary>
     public partial class FoodInventoryWindow : Window
     {
+        public class ProductsInventoryRecord
+        {
+            [Key]
+            public int Number { get; set; }
+            public string Name { get; set; }
+            public string Amount { get; set; }
+            public decimal Price { get; set; }
+            public string Currency { get; set; }
+        }
+        public ObservableCollection<ProductsInventoryRecord> ProductsInventoryCollection { get; set; }
         public FoodInventoryWindow()
         {
             InitializeComponent();
             MainHeader.SetWindowName("2. Inventory of products for sale");
+
+            ProductsInventoryCollection = [
+                new ProductsInventoryRecord(){Number = 1, Name = "Tea", Amount = "100 ml", Price = 10, Currency = "TL", },
+                new ProductsInventoryRecord(){Number = 2, Name = "Coffee", Amount = "200 ml", Price = 30, Currency = "TL", },
+                new ProductsInventoryRecord(){Number = 3, Name = "Ice cream", Amount = "30 gramm", Price = 30, Currency = "TL",},
+            ];
+
+            ProductsInventoryDataGrid.ItemsSource = ProductsInventoryCollection;
         }
     }
 }
